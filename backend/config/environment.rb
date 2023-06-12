5ebbe0445fc96f320e5130a3780ff87a0f296292
John <john.muchuku@student.moringaschool.com>
2023-06-12 13:22:40 +0300

    deploy

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -2,5 +2,12 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
-
+# Database configrurations
+configure do
+    set :database, {
+      adapter: 'sqlite3',
+      database: 'db/development.sqlite3'
+    }
+  end
+  
 require_all 'app'
